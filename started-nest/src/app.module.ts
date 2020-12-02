4a5a51e7769c991810c5bb08be6550db8a9b3549
José Murillo <jooseemurillo@gmail.com>
2020-12-02 09:39:04 -0300

    Remove comments app.module🤍

--- started-nest/src/app.module.ts
+++ started-nest/src/app.module.ts
@@ -2,8 +2,6 @@ import { ItemsModule } from './items/items.module';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-// import { ItemsController } from './items/items.controller';
-// import { ItemsService } from './items/items.service';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
