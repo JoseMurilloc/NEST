3eeabc4ee1c25a3fe761596f675dc25a4eb41317
José Murillo <jooseemurillo@gmail.com>
2020-12-02 15:36:29 -0300

    🔥 feat update and delete user

---
+++ connection-postgres-nest/src/users/dtos/UserDtos.ts
@@ -0,0 +1,5 @@
+export default class UserDtos {
+  name: string;
+  email: string;
+  password: string;
+}
--- connection-postgres-nest/src/users/users.controller.ts
+++ connection-postgres-nest/src/users/users.controller.ts
@@ -1,7 +1,18 @@
 import { User } from './../entities/UserModel';
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Res,
+} from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Response } from 'express';
+// import UserDtos from './dtos/UserDtos';
 
 @Controller('users')
 export class UsersController {
@@ -24,5 +35,21 @@ export class UsersController {
   async store(@Body() body: User): Promise<User> {
     const user = this.userRepository.create(body);
     return await this.userRepository.save(user);
+  }
+
+  @Put(':id')
+  async update(@Param() id: string, @Body() body: User): Promise<User> {
+    await this.userRepository.findOneOrFail(id);
+    await this.userRepository.update(id, body);
+    return await this.userRepository.findOneOrFail(id);
+  }
+
+  @Delete(':id')
+  async delete(
+    @Param() id: string,
+    @Res() response: Response,
+  ): Promise<Response> {
+    await this.userRepository.delete(id);
+    return response.json();
   }
 }
